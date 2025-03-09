308cf7c4229cf80237d9dd7a0db610067600a146
bthntncr81 <93138181+bthntncr81@users.noreply.github.com>
2025-03-09 13:41:16 +0300

    changes

--- src/app/checkout/pay2/signal-r.service.ts
+++ src/app/checkout/pay2/signal-r.service.ts
@@ -16,7 +16,7 @@ export class SignalRService {
 
   startConnection = () => {
     this.hubConnection = new signalR.HubConnectionBuilder()
-      .withUrl("ws://payment.scald.shop/pay-hub", {
+      .withUrl("wss://payment.scald.shop/pay-hub", {
         skipNegotiation: true,
         transport: signalR.HttpTransportType.WebSockets,
       })
